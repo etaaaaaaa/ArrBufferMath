820db7d16dca6e2fd4bfd7862801b96432695924
Alexandra Maltseva <etaaaaaaa@gmail.com>
2022-01-30 21:25:16 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+export default class Magican {
+  constructor(attack) {
+    this.attack = attack;
+    this.stoned = false;
+    this.distance;
+  }
+
+  get getAttack() {
+    if(!this.distance) return this.attack;
+    if(this.distance > 11) return 0;
+    let item = Number((this.distance - 1) + "0");
+    let precent = item/100;
+    if (this.stoned === true) {
+      return Math.round((this.attack - (this.attack * precent)) - (Math.log2(this.distance) * 5));
+    } else {
+      return this.attack - (this.attack * precent);
+    }
+  }
+
+  set setAttack(value) {
+    this.distance = value;
+  }
+
+  get getStoned() {
+    return this.stoned;
+  }
+
+  set setStoned(value) {
+    if (typeof (value) === 'boolean') { this.stoned = value; }
+  }
+}
+
+export class Daemon extends Magican {
+
+}
